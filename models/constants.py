5dddb866f81a10627db6a588218ce11eb42341e9
David Lopes <david.lopes@dynatrace.com>
2020-06-06 18:35:22 -0300

    Move namespace creation to python

---
+++ models/constants.py
@@ -0,0 +1,16 @@
+wbemCimtypeSint16 = 2
+wbemCimtypeSint32 = 3
+wbemCimtypeReal32 = 4
+wbemCimtypeReal64 = 5
+wbemCimtypeString = 8
+wbemCimtypeBoolean = 11
+wbemCimtypeObject = 13
+wbemCimtypeSint8 = 16
+wbemCimtypeUint8 = 17
+wbemCimtypeUint16 = 18
+wbemCimtypeUint32 = 19
+wbemCimtypeSint64 = 20
+wbemCimtypeUint64 = 21
+wbemCimtypeDateTime = 101
+wbemCimtypeReference = 102
+wbemCimtypeChar16 = 103
